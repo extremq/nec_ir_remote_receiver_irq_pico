7cca58c671e76a0d03c4995e4f4e62bedfad4a1c
extremq <45830561+extremq@users.noreply.github.com>
2022-10-15 00:29:33 +0300

    Make use of extern.

--- ir_rx_irq.h
+++ ir_rx_irq.h
@@ -17,6 +17,28 @@
 #define MAXIMUM_SPACE 15000
 #define REPEAT_SPACE  11250
 
+
+/**
+ * @brief Representation of the pulses by time in microseconds.
+ */
+struct _ir_data {
+    size_t cnt; /**< Used for keeping track of pulse length. */
+    uint64_t rises[34]; /**< Used for keeping track of timings. Has time of each pulse in microseconds. */
+};
+
+extern struct _ir_data ir_data;
+
+/**
+ * @brief Used for repeat codes
+ */
+extern uint16_t __last_address;
+extern uint16_t __last_command;
+
+/**
+ * @brief The user's function. 
+ */
+extern void (*user_function_callback) (uint16_t address, uint16_t command, int type);
+
 /**
  * @brief Function that zeroes the ir_data struct.
  */
